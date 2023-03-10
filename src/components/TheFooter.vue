07068e0a487aa7c14f088ef533397e360da5356c
krsnamara <martinj.fitzpatrick@gmail.com>
2023-03-10 12:51:05 -0500

    fixed nav, footer border top and bk color removed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,15 +1,23 @@
 <template>
     <footer>
-        <div class="social-links">
-            <div class="link">
-                <a href="https://github.com/krsnamara">
-                    <img src="https://www.svgrepo.com/show/68072/github-logo-face.svg" alt="">
-                </a>
+        <div class="footer-wrapper">
+            <div class="footer-links">
+                <router-link class="router-link" to="/">home</router-link>
+                <router-link class="router-link" to="/projects">projects</router-link>
+                <!-- <router-link class="router-link" to="/resume">resume</router-link> -->
+                <router-link class="router-link" to="/blog">blog</router-link>
             </div>
-            <div class="link">
-                <a href="https://www.linkedin.com/in/martinj-fitzpatrick/">
-                    <img src="https://www.svgrepo.com/show/390272/linkedin-linked-in.svg" alt="">
-                </a>
+            <div class="social-links">
+                <div class="link">
+                    <a href="https://github.com/krsnamara">
+                        <img src="https://www.svgrepo.com/show/68072/github-logo-face.svg" alt="">
+                    </a>
+                </div>
+                <div class="link">
+                    <a href="https://www.linkedin.com/in/martinj-fitzpatrick/">
+                        <img src="https://www.svgrepo.com/show/390272/linkedin-linked-in.svg" alt="">
+                    </a>
+                </div>
             </div>
         </div>
     </footer>
@@ -22,8 +30,30 @@
 <style scoped>
     footer{
         margin-top: auto;
-        background-color: #f3f3f3;
-        /* position: fixed;  */
+        border-top: solid 3px #f3f3f3;
+        position:relative; 
+    }
+    .footer-links{
+        display: flex;
+        align-items:center;
+        justify-content: center;
+        gap: 15px;
+        /* padding-right: 80px; */
+    }
+    .footer-wrapper{
+        display: grid;
+        grid-template-columns: 80% 20%;
+    }
+    footer a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+    footer a:hover {
+        font-weight: bold;
+        color: #1b1b1b;
+    }
+    footer a.router-link-exact-active {
+         text-decoration: underline;
     }
     .social-links{
         display: flex;
